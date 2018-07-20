2ddafcd41ca44cec184939480c3a225cfbcad811
Viikable <tehkovarma@hotmail.com>
2018-07-20 17:36:19 +0300

    added a few teleportable areas for testing

--- VREpisode1/Assets/OwnStuff/Scripts/PlayerFinding.cs
+++ VREpisode1/Assets/OwnStuff/Scripts/PlayerFinding.cs
@@ -6,11 +6,15 @@
 
     public class PlayerFinding : MonoBehaviour
     {
+        protected Transform headset;
+        protected Transform playArea;
 
         // Use this for initialization
         void Start()
         {
             VRTK_PlayerObject.SetPlayerObject(gameObject, VRTK_PlayerObject.ObjectTypes.CameraRig);
+            headset = VRTK_SharedMethods.AddCameraFade();
+            playArea = VRTK_DeviceFinder.PlayAreaTransform();
         }
 
         // Update is called once per frame
