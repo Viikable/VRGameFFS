43f6a038c06087da4ab4b66fef80d3a5920cf604
Viikable <tehkovarma@hotmail.com>
2018-09-09 20:01:49 +0300

    water is a lot better, lantern lights up when used, chest opens and adjusted the friction of opening locker doors

--- VREpisode1/Assets/OwnStuff/Scripts/WaterMovement.cs
+++ VREpisode1/Assets/OwnStuff/Scripts/WaterMovement.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterMovement : MonoBehaviour {
-
+    public static bool WaterRises;
 	// Use this for initialization
 	void Start () {
-		
+        WaterRises = false;
 	}
-	
+    public void TouchedLantern()
+    {
+        Debug.Log("Lantern is touched, let the waters rise!");
+        
+    }
 	// Update is called once per frame
 	void Update () {
-        this.transform.Translate(Vector3.up*0.01f);
+        if (WaterRises)
+        {
+            this.transform.Translate(Vector3.up * 0.005f);
+        }
+        
 	}
 }
