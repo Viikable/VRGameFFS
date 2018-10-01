bedb55d0323d6f27fcb64bfea6daa91afedd11a8
Viikable <tehkovarma@hotmail.com>
2018-10-01 22:14:13 +0300

    thang change the tune

--- VREpisode1/Assets/OwnStuff/Scripts/ConveyorBeltController.cs
+++ VREpisode1/Assets/OwnStuff/Scripts/ConveyorBeltController.cs
@@ -6,15 +6,20 @@ public class ConveyorBeltController : MonoBehaviour {
     Animator anim;
     Animator anim2;
     Animator anim3;
+    AudioSource ConveyorAudio;
+    AudioSource ConveyorAudio2;
+    AudioSource ConveyorAudio3;
     Animator animDown;
     Animation Animation;
-    public static bool OddPress = true;
-    public static bool EvenPress = false;
+    public static bool NotPlaying = true;
     public static bool PressedScreen1 = true;
     public bool PressedScreen2 = false;
     public static bool PressedScreen3 = false;
     // Use this for initialization
     void Start () {
+        ConveyorAudio = GameObject.Find("ConveyorAudio").GetComponent<AudioSource>();
+        ConveyorAudio2 = GameObject.Find("ConveyorAudio2").GetComponent<AudioSource>();
+        ConveyorAudio3 = GameObject.Find("ConveyorAudio3").GetComponent<AudioSource>();
         anim = GameObject.Find("Conveyor_belt_Animated").GetComponent<Animator>();
         anim2 = GameObject.Find("Conveyor_belt_Animated2").GetComponent<Animator>();
         anim3 = GameObject.Find("Conveyor_belt_Animated3").GetComponent<Animator>();
@@ -26,32 +31,47 @@ public class ConveyorBeltController : MonoBehaviour {
     void Update () {
 		if (PressedScreen1)
         {
+            //starts the sound and movement for all normal conveyorbelts
+            if (NotPlaying)
+            {
+                NotPlaying = false;
+                ConveyorAudio.Play();
+                ConveyorAudio2.Play();
+                ConveyorAudio3.Play();
+            }
             anim.SetBool("Start", true);
             anim2.SetBool("Start", true);
             anim3.SetBool("Start", true);
+            Animation["ConveyorBeltMovement"].speed = 2f;
+            Animation["ConveyorDisappear"].speed = 2f;
+            Animation["ConveyorMovementDown"].speed = 2f;
         }
         
         if (PressedScreen2)
         {
+            //makes the middle conveyor belt go down and sets it speed to normal if it was stopped
             anim.SetBool("Open", true);
             animDown.SetBool("Open", true);
+            Animation["ConveyorMovementDown"].speed = 1f;
         }
-        if (PressedScreen3 && OddPress)
+        if (PressedScreen3)
         {
-            OddPress = false;
-            EvenPress = true;
+            //stops the sound and movement of all conveyorbelts
+            if (!NotPlaying)
+            {
+                ConveyorAudio.Stop();
+                ConveyorAudio2.Stop();
+                ConveyorAudio3.Stop();
+                NotPlaying = true;
+            }
             Animation["ConveyorBeltMovement"].speed = 0f;
             Animation["ConveyorDisappear"].speed = 0f;
             Animation["ConveyorMovementDown"].speed = 0f;
             
         }
-        if (PressedScreen3 && EvenPress)
+        if (PressedScreen3)
         {
-            OddPress = true;
-            EvenPress = false;
-            Animation["ConveyorBeltMovement"].speed = 1f;
-            Animation["ConveyorDisappear"].speed = 1f;
-            Animation["ConveyorMovementDown"].speed = 1f;
+           //melt the metal animation
 
         }
     }
