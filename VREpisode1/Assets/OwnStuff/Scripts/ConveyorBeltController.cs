764a10e89d8e7022032136eba4deae59ae59f7f2
Viikable <tehkovarma@hotmail.com>
2018-10-01 21:34:24 +0300

    I did it, I fucking did it

--- VREpisode1/Assets/OwnStuff/Scripts/ConveyorBeltController.cs
+++ VREpisode1/Assets/OwnStuff/Scripts/ConveyorBeltController.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 
 public class ConveyorBeltController : MonoBehaviour {
     Animator anim;
+    Animator anim2;
+    Animator anim3;
+    Animator animDown;
+    Animation Animation;
+    public static bool OddPress = true;
+    public static bool EvenPress = false;
     public static bool PressedScreen1 = true;
-    public static bool PressedScreen2 = false;
+    public bool PressedScreen2 = false;
     public static bool PressedScreen3 = false;
     // Use this for initialization
     void Start () {
-        anim = this.GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        anim = GameObject.Find("Conveyor_belt_Animated").GetComponent<Animator>();
+        anim2 = GameObject.Find("Conveyor_belt_Animated2").GetComponent<Animator>();
+        anim3 = GameObject.Find("Conveyor_belt_Animated3").GetComponent<Animator>();
+        animDown = GameObject.Find("Conveyor_belt_AnimatedDown").GetComponent<Animator>();
+        //ConveyorMovement = anim.GetComponent<Animation>();
+    }
+   
+    // Update is called once per frame
+    void Update () {
 		if (PressedScreen1)
         {
             anim.SetBool("Start", true);
+            anim2.SetBool("Start", true);
+            anim3.SetBool("Start", true);
         }
+        
         if (PressedScreen2)
         {
             anim.SetBool("Open", true);
+            animDown.SetBool("Open", true);
         }
-	}
+        if (PressedScreen3 && OddPress)
+        {
+            OddPress = false;
+            EvenPress = true;
+            Animation["ConveyorBeltMovement"].speed = 0f;
+            Animation["ConveyorDisappear"].speed = 0f;
+            Animation["ConveyorMovementDown"].speed = 0f;
+            
+        }
+        if (PressedScreen3 && EvenPress)
+        {
+            OddPress = true;
+            EvenPress = false;
+            Animation["ConveyorBeltMovement"].speed = 1f;
+            Animation["ConveyorDisappear"].speed = 1f;
+            Animation["ConveyorMovementDown"].speed = 1f;
+
+        }
+    }
 }
