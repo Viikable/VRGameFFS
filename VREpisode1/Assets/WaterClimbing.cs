dae7caf2a4f54bca15c640d74cb91f64919be977
Viikable <tehkovarma@hotmail.com>
2018-10-09 16:07:25 +0300

    swimming works better

---
+++ VREpisode1/Assets/WaterClimbing.cs
@@ -0,0 +1,39 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using System.Collections;
+
+    public class WaterClimbing : VRTK_InteractableObject
+    {
+        public override void Grabbed(VRTK_InteractGrab grabbingObject)
+        {
+            base.Grabbed(grabbingObject);
+            //ToggleKinematics(false);
+            StartCoroutine(UngrabQuickly());
+            Debug.Log("grabbedwater");
+        }
+        IEnumerator UngrabQuickly()
+        {
+            Debug.Log("beforewait");
+            print(Time.time);
+            yield return new WaitForSeconds(0.5f);
+            print(Time.time);
+            Debug.Log("afterwait");
+            base.Ungrabbed();
+        }
+
+        //public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject)
+        //{
+        //    base.Ungrabbed(previousGrabbingObject);
+        //    ToggleKinematics(true);
+        //}
+
+        //private void ToggleKinematics(bool state)
+        //{
+        //    foreach (Rigidbody rigid in transform.parent.GetComponentsInChildren<Rigidbody>())
+        //    {
+        //        rigid.isKinematic = state;
+        //    }
+        //}
+    }
+}
