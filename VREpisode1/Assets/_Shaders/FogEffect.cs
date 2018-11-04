5acf2839f3f9eeeb3a40679df46a7284ee603a72
Viikable <tehkovarma@hotmail.com>
2018-11-04 22:09:11 +0200

    Tessellation complete, made a dummy waterbed with fog

--- VREpisode1/Assets/_Shaders/FogEffect.cs
+++ VREpisode1/Assets/_Shaders/FogEffect.cs
@@ -3,36 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
-public class UnderWaterEffect : MonoBehaviour {
-
+public class FogEffect : MonoBehaviour {
     public Material _mat;
-
-
-    [Range(0.001f, 0.1f)]
-    public float _pixelOffset;
-    [Range(0.1f, 20f)]
-    public float _noiseScale;
-    [Range(0.1f, 20f)]
-    public float _noiseFrequency;
-    [Range(0.1f, 30f)]
-    public float _noiseSpeed;
-
+    public Color _fogColor;
     public float _depthStart;
     public float _depthDistance;
+
     // Use this for initialization
     void Start () {
-		
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        _mat.SetFloat("_NoiseFrequency", _noiseFrequency);
-        _mat.SetFloat("_NoiseSpeed", _noiseSpeed);
-        _mat.SetFloat("_NoiseScale", _noiseScale);
-        _mat.SetFloat("_PixelOffset", _pixelOffset);
+        _mat.SetColor("_FogColor", _fogColor);
         _mat.SetFloat("_DepthStart", _depthStart);
         _mat.SetFloat("_DepthDistance", _depthDistance);
-
     }
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
