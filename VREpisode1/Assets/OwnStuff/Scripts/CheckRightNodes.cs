2c1b323839b480fca33a843644d760c9242557ba
Chung Pham <chungphamhn@gmail.com>
2018-12-06 15:55:33 +0200

    save work

---
+++ VREpisode1/Assets/OwnStuff/Scripts/CheckRightNodes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckRightNodes : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject firstNodeRightObject;
+    [SerializeField]
+    private GameObject secondNodeRightObject;
+    [SerializeField]
+    private GameObject thirdNodeRightObject;
+
+    [SerializeField]
+    private GameObject Node1CurrentObject;
+    [SerializeField]
+    private GameObject Node2CurrentObject;
+    [SerializeField]
+    private GameObject Node3CurrentObject;
+	
+	// Update is called once per frame
+	void Update () {
+        Node1CurrentObject = GameObject.Find("SnapDropZone").GetComponent<VRTK.VRTK_SnapDropZone>().GetCurrentSnappedObject();
+        Node2CurrentObject = GameObject.Find("SnapDropZone2").GetComponent<VRTK.VRTK_SnapDropZone>().GetCurrentSnappedObject();
+        Node3CurrentObject = GameObject.Find("SnapDropZone3").GetComponent<VRTK.VRTK_SnapDropZone>().GetCurrentSnappedObject();
+
+        if(firstNodeRightObject == Node1CurrentObject && secondNodeRightObject == Node2CurrentObject && thirdNodeRightObject == Node3CurrentObject) {
+            Debug.Log("Right Order");
+        }
+	}
+}
