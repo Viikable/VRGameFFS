fd0e366ac46a95e308072bc8b95dda672f456714
Viikable <tehkovarma@hotmail.com>
2018-12-11 17:09:59 +0200

    oxygen system, recognizing different bodypart colliders

--- VREpisode1/Assets/OwnStuff/Scripts/GravityModifier.cs
+++ VREpisode1/Assets/OwnStuff/Scripts/GravityModifier.cs
@@ -8,12 +8,20 @@ using UnityEngine;
 
 
     public class GravityModifier : MonoBehaviour
-    {
+    { /*
         [Tooltip("Have we touched the water surface yet or not")]
         public bool TouchedWater;
 
         [Tooltip("Have we left the water or not")]
         public bool ExitedWater;
+
+        [SerializeField]
+        [Tooltip("Time when player enters the water")]
+        float timeWhenGotUnderwater;
+
+        [SerializeField]
+        [Tooltip("How much oxygen the player has left")]
+        float oxygenTimer;
 
         [Header("Water Hitting sound")]
         [Tooltip("The water hitting sound")]
@@ -25,17 +33,21 @@ using UnityEngine;
         {
             headsetbody = this.GetComponent<Rigidbody>();
             TouchedWater = false;
-       
+            oxygenTimer = 30f;
 
         }
 
         private void OnTriggerEnter(Collider water)
         {
+            GameObject footCollider = GameObject.Find("[VRTK][AUTOGEN][FootColliderContainer]");
+
             if (water.name == "GrabbableWater")       //just to check which object the rigidbody attached to the camerarig collided with
             {
+                timeWhenGotUnderwater = Time.time;
+                Debug.Log(timeWhenGotUnderwater);
                 TouchedWater = true;                            //whenever we want the gravity to return to normal we can just change the bool back to false
                 Debug.Log("Touched the water");
-                
+
                 Splash.Play();
                 this.GetComponentInChildren<UnderWaterEffect>().enabled = true;
 
@@ -51,33 +63,37 @@ using UnityEngine;
             }
         }
         // Update is called once per frame
-        void FixedUpdate()
-        {
-            //Debug.Log(headsetbody.velocity.y+ "Y");
-            //Debug.Log(headsetbody.velocity.x +"X");
-            //Debug.Log(headsetbody.velocity.z+ "Z");
-           
+        void Update()
+        {          
             if (TouchedWater)
             {
+                if (oxygenTimer < Time.time - timeWhenGotUnderwater)
+                {
+                    Debug.Log("drowned");
+                    Debug.Log(Time.time);
+                    //player dies here, lose control, sink to bottom, fade to black
+
+                }
+
                 headsetbody.useGravity = false;
 
                 headsetbody.AddForce(Physics.gravity * headsetbody.mass / 10);
-                //GameObject.Find("Either Controller - X Axis Slide - Y Axis Slide").GetComponent<VRTK_SlideObjectControlAction>().maximumSpeed = 0.5f;
-                if (headsetbody.velocity.y >= 0)
-                {
-                    Debug.Log("now changes");
-                    headsetbody.AddForce(new Vector3(0,3,0));
-                }
-                else
-                {
-                    return;
-                }
+                
+                //if (headsetbody.velocity.y >= 0)
+                //{
+                //    Debug.Log("now changes");
+                //    headsetbody.AddForce(new Vector3(0,-3,0));
+                //}
+                //else
+                //{
+                //    return;
+                //}
 
             }
             else
             {
                 headsetbody.useGravity = true;
             }
-        }
-    }
+        } */
+    } 
 }
--- VREpisode1/Assets/OwnStuff/Scripts/WaterMovement.cs
+++ VREpisode1/Assets/OwnStuff/Scripts/WaterMovement.cs
@@ -2,29 +2,111 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterMovement : MonoBehaviour {
+public class WaterMovement : MonoBehaviour
+{
     [SerializeField]
     [Tooltip("Is the water rising right now")]
     private bool waterRises;
+    [Tooltip("Have we touched the water surface yet or not")]
+    public bool TouchedWater;
 
-	void Start () {
+    [Tooltip("Have we left the water or not")]
+    public bool ExitedWater;
+
+    [SerializeField]
+    [Tooltip("Time when player enters the water")]
+    float timeWhenGotUnderwater;
+
+    [SerializeField]
+    [Tooltip("How much oxygen the player has left")]
+    float oxygenTimer;
+
+    [Header("Water Hitting sound")]
+    [Tooltip("The water hitting sound")]
+    public AudioSource Splash;
+
+    public GameObject headSet;
+    public Rigidbody headsetbody;
+    public Collider feet;
+    public Collider head;
+
+    void Start()
+    {
+        TouchedWater = false;
+        oxygenTimer = 30f;
         waterRises = false;
-	}
+        headSet = GameObject.Find("[VRTK_SDKManager]").transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
+        headsetbody = null;
+        feet = null;
+        head = null;
+    }
+
+    //   void Start () {
+    //       waterRises = false;
+    //}
     public void TouchedLantern()
     {
         Debug.Log("Lantern is touched, let the waters rise!");
-        
-    }
 
-    public bool WaterRises
+    }
+    private void OnTriggerEnter(Collider hitCollider)
     {
-        get { return waterRises;  }
+        
+        if (hitCollider == feet)       //just to check which object the rigidbody attached to the camerarig collided with
+        {          
+            TouchedWater = true;                            //whenever we want the gravity to return to normal we can just change the bool back to false
+            Debug.Log("feet entered water");
+            Splash.Play();
+            headSet.GetComponentInChildren<UnderWaterEffect>().enabled = true;
 
-        set { waterRises = value;  }
+        }
+        if (hitCollider = head)
+        {
+            Debug.Log("head entered water");
+            timeWhenGotUnderwater = Time.time;
+            Debug.Log(timeWhenGotUnderwater);
+        }
     }
-  
-    // Update is called once per frame
-    void Update () {
+    private void OnTriggerExit(Collider hitCollider)
+    {
+        if (hitCollider == feet)
+        {           
+            Debug.Log("feet exited water");
+            headSet.GetComponentInChildren<UnderWaterEffect>().enabled = false;
+        }
+        if (hitCollider == head)
+        {
+            Debug.Log("head exited water");
+            TouchedWater = false;
+        }
+    }
+    void Update()
+    {
+        if (Time.time >= 0.5f)
+        {
+            feet = headSet.transform.GetChild(3).GetChild(0).GetComponent<Collider>();   //finds the collider child for feet
+            head = headSet.transform.GetChild(2).GetChild(3).GetComponent<Collider>();    //finds the collider child for head
+        }
+        headsetbody = headSet.GetComponent<Rigidbody>();
+        if (TouchedWater)
+        {
+            if (oxygenTimer < Time.time - timeWhenGotUnderwater)
+            {
+                Debug.Log("drowned");
+                Debug.Log(Time.time);
+                //player dies here, lose control, sink to bottom, fade to black
+
+            }
+
+            headsetbody.useGravity = false;
+
+            headsetbody.AddForce(Physics.gravity * headsetbody.mass / 10);
+        }
+        else if (headsetbody != null)
+        {
+            headsetbody.useGravity = true;
+        }
+
         if (WaterRises)
         {
             this.transform.Translate(Vector3.up * 0.005f);
@@ -35,6 +117,26 @@ public class WaterMovement : MonoBehaviour {
             this.transform.Translate(Vector3.up * 0f);
             //Debug.Log("waterdown");
         }
-        
-	}
+    }
+    public bool WaterRises
+    {
+        get { return waterRises; }
+
+        set { waterRises = value; }
+    }
 }
+    // Update is called once per frame
+ //   void Update () {
+ //       if (WaterRises)
+ //       {
+ //           this.transform.Translate(Vector3.up * 0.005f);
+ //           Debug.Log("waterup");
+ //       }
+ //       else
+ //       {
+ //           this.transform.Translate(Vector3.up * 0f);
+ //           //Debug.Log("waterdown");
+ //       }
+        
+	//}
+
