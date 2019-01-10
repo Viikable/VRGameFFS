00344b6daac3729cabf0402125aeb5a905292c77
Viikable <tehkovarma@hotmail.com>
2019-01-10 15:38:21 +0200

    starting the melting job

---
+++ VREpisode1/Assets/MelterEnterTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MelterEnterTrigger : MonoBehaviour {
+    [SerializeField]
+    private int amountOfMeltedObjects;
+
+    private void Start()
+    {
+        amountOfMeltedObjects = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ConveyorBeltMetal"))
+        {
+            amountOfMeltedObjects += 1;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ConveyorBeltMetal"))
+        {
+            amountOfMeltedObjects -= 1;
+        }
+    }
+    private void Update()
+    {
+        if (amountOfMeltedObjects >= 5)
+        {
+            //ready the melting button/lever
+        }
+    }
+}
