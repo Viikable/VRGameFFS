0c45867584bbd748c86dbbcc82b080a86abf1f79
Viikable <tehkovarma@hotmail.com>
2019-01-16 14:11:03 +0200

    melting metal scaling works

--- VREpisode1/Assets/OwnStuff/Scripts/MetalHitsTheFan.cs
+++ VREpisode1/Assets/OwnStuff/Scripts/MetalHitsTheFan.cs
@@ -14,12 +14,12 @@ public class MetalHitsTheFan : MonoBehaviour {
 
     private void Awake()
     {
-        MetalSounds1 = GameObject.Find("MetalSounds1").GetComponent<AudioSource>();
-        MetalSounds2 = GameObject.Find("MetalSounds2").GetComponent<AudioSource>();
-        MetalSounds3 = GameObject.Find("MetalSounds3").GetComponent<AudioSource>();
-        MetalSounds4 = GameObject.Find("MetalSounds4").GetComponent<AudioSource>();
-        MetalSounds5 = GameObject.Find("MetalSounds5").GetComponent<AudioSource>();
-        MetalSounds6 = GameObject.Find("MetalSounds6").GetComponent<AudioSource>();
+        MetalSounds1 = transform.Find("MetalSounds1").GetComponent<AudioSource>();
+        MetalSounds2 = transform.Find("MetalSounds2").GetComponent<AudioSource>();
+        MetalSounds3 = transform.Find("MetalSounds3").GetComponent<AudioSource>();
+        MetalSounds4 = transform.Find("MetalSounds4").GetComponent<AudioSource>();
+        MetalSounds5 = transform.Find("MetalSounds5").GetComponent<AudioSource>();
+        MetalSounds6 = transform.Find("MetalSounds6").GetComponent<AudioSource>();
         randomizer = 0;
         insideTheMelter = false;
     }
@@ -60,17 +60,19 @@ public class MetalHitsTheFan : MonoBehaviour {
             }
         }
     }
-   
-    private void Update()
-    {
-        var newScale = Mathf.Lerp(1, 0.5f, Time.deltaTime / 10);
-        randomizer = Random.Range(1,7);      //so between 1-6
 
+    static float t = 0.0f;
+       
+    private void Update()
+    {       
+        randomizer = Random.Range(1,7);      //so between 1-6
+        float newScale = Mathf.Lerp(1, 0.1f, t);
         if (insideTheMelter)
         {
             //scale this specific piece of scrap metal slowly down and at the same time another animation rises the lava
             
             transform.localScale = new Vector3(newScale, newScale, newScale);
+            t += 0.1f * Time.deltaTime;
         }
     }
 }
