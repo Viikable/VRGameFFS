0fbae3ceb89c3658bddb41f18eabc0189788b376
Viikable <tehkovarma@hotmail.com>
2019-02-06 20:19:02 +0200

    ledgetry + colliders

---
+++ VREpisode1/Assets/OwnStuff/Scripts/Ledge.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Ledge : MonoBehaviour {
+    VRTK_SnapDropZone ledgeSnap;
+    int i;
+
+	// Use this for initialization
+	void Start () {
+        ledgeSnap = GameObject.Find("MelterWalkableLedgeSnapZone").GetComponent<VRTK_SnapDropZone>();
+        i = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ledgeSnap.GetCurrentSnappedObject() == this && i == 0)
+        {
+            GetComponent<VRTK_InteractableObject>().isGrabbable = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            i++;
+        }
+	}
+}
