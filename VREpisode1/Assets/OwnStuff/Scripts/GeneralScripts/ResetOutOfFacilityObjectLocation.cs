8f29cfeef58b8ec5a46852ba1ff2d2970932c449
Viikable <tehkovarma@hotmail.com>
2019-10-22 15:51:26 +0300

    created LocationReset scripts for interactable objects if they hit the seabed

---
+++ VREpisode1/Assets/OwnStuff/Scripts/GeneralScripts/ResetOutOfFacilityObjectLocation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ResetOutOfFacilityObjectLocation : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<VRTK_InteractableObject>() != null)
+        {
+            if (other.GetComponent<LocationReset>() != null)
+            {
+            other.GetComponent<LocationReset>().ResetLocation();
+            }
+        }
+        else if (other.transform.parent.GetComponent<VRTK_InteractableObject>() != null)
+        {
+            other.transform.parent.GetComponent<LocationReset>().ResetLocation();
+        }
+    }
+}
