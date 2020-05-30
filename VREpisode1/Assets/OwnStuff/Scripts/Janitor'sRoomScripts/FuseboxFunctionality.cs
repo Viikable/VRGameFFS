8f0bf02432cdb6f8ae98a77a0c900e1c1ef3ce41
Viikable <tehkovarma@hotmail.com>
2020-05-30 17:42:41 +0300

    Lights

--- VREpisode1/Assets/OwnStuff/Scripts/Janitor'sRoomScripts/FuseboxFunctionality.cs
+++ VREpisode1/Assets/OwnStuff/Scripts/Janitor'sRoomScripts/FuseboxFunctionality.cs
@@ -10,39 +10,70 @@ public class FuseboxFunctionality : MonoBehaviour {
     public VRTK_SnapDropZone MaintenanceMachinery;
     public VRTK_SnapDropZone MaintenanceDoors;
 
+    GameObject MaintenanceLightsContainer;
+
+    public bool MaintenanceDoorsOpen;
+
     //Main Facility
     public VRTK_SnapDropZone MainFacilityLights;
     public VRTK_SnapDropZone MainFacilityMachinery;
     public VRTK_SnapDropZone MainFacilityDoors;
+
+    GameObject MainFacilityLightsContainer;
+
+    public bool MainFacilityDoorsOpen;
 
     //Bridge
     public VRTK_SnapDropZone BridgeLights;
     public VRTK_SnapDropZone BridgeMachinery;
     public VRTK_SnapDropZone BridgeDoors;
 
+    GameObject BridgeLightsContainer;
+
+    public bool BridgeDoorsOpen;
+
     //Melter
     public VRTK_SnapDropZone MelterLights;
     public VRTK_SnapDropZone MelterMachinery;
     public VRTK_SnapDropZone MelterDoors;
 
-    
+    GameObject MelterLightsContainer;
+
+    public bool MelterDoorsOpen;
+
+
     void Start () {
 
+
+        //Maintenance
         MaintenanceLights = transform.Find("MaintenanceAreaLights").GetComponent<VRTK_SnapDropZone>();
         MaintenanceMachinery = transform.Find("MaintenanceAreaMachinery").GetComponent<VRTK_SnapDropZone>();
         MaintenanceDoors = transform.Find("MaintenanceAreaDoors").GetComponent<VRTK_SnapDropZone>();
 
+        MaintenanceLightsContainer = GameObject.Find("MaintenanceLightsContainer");
+
+        MaintenanceDoorsOpen = true;
+
+        //Main facility
         MainFacilityLights = transform.Find("MainFacilityLights").GetComponent<VRTK_SnapDropZone>();
         MainFacilityMachinery = transform.Find("MainFacilityMachinery").GetComponent<VRTK_SnapDropZone>();
         MainFacilityDoors = transform.Find("MainFacilityDoors").GetComponent<VRTK_SnapDropZone>();
 
+        MainFacilityLightsContainer = GameObject.Find("MainFacilityLightsContainer");
+
+        //Bridge
         BridgeLights = transform.Find("BridgeLights").GetComponent<VRTK_SnapDropZone>();
         BridgeMachinery = transform.Find("BridgeMachinery").GetComponent<VRTK_SnapDropZone>();
         BridgeDoors = transform.Find("BridgeDoors").GetComponent<VRTK_SnapDropZone>();
 
+        BridgeLightsContainer = GameObject.Find("BridgeLightsContainer");
+
+        //Melter
         MelterLights = transform.Find("MelterLights").GetComponent<VRTK_SnapDropZone>();
         MelterMachinery = transform.Find("MelterMachinery").GetComponent<VRTK_SnapDropZone>();
         MelterDoors = transform.Find("MelterDoors").GetComponent<VRTK_SnapDropZone>();
+
+        MelterLightsContainer = GameObject.Find("MelterLightsContainer");
 
     }
 	
@@ -60,37 +91,77 @@ public class FuseboxFunctionality : MonoBehaviour {
         if (MaintenanceLights.GetCurrentSnappedObject() != null)
         {
             //turn Maintenancelights on
+            foreach (Light light in MaintenanceLightsContainer.GetComponentsInChildren<Light>())
+            {
+                light.intensity = 2f;
+                light.color = Color.white;
+            }
         }
         else
         {
             //turn Maintenancelights off
+            foreach (Light light in MaintenanceLightsContainer.GetComponentsInChildren<Light>())
+            {
+                light.intensity = 1f;
+                light.color = Color.red;
+            }
         }
 
         if (MainFacilityLights.GetCurrentSnappedObject() != null)
         {
             //turn MainFacilitylights on
+            foreach (Light light in MainFacilityLightsContainer.GetComponentsInChildren<Light>())
+            {
+                light.intensity = 3f;
+                light.color = Color.white;
+            }
         }
         else
         {
             //turn MainFacilitylights off
+            foreach (Light light in MainFacilityLightsContainer.GetComponentsInChildren<Light>())
+            {
+                light.intensity = 1f;
+                light.color = Color.red;
+            }
         }
 
         if (BridgeLights.GetCurrentSnappedObject() != null)
         {
             //turn Bridgelights on
+            foreach (Light light in BridgeLightsContainer.GetComponentsInChildren<Light>())
+            {
+                light.intensity = 3f;
+                light.color = Color.white;
+            }
         }
         else
         {
             //turn Bridgelights off
+            foreach (Light light in BridgeLightsContainer.GetComponentsInChildren<Light>())
+            {
+                light.intensity = 1f;
+                light.color = Color.red;
+            }
         }
 
         if (MelterLights.GetCurrentSnappedObject() != null)
         {
             //turn Melterlights on
+            foreach (Light light in MelterLightsContainer.GetComponentsInChildren<Light>())
+            {
+                light.intensity = 2f;
+                light.color = Color.white;
+            }
         }
         else
         {
             //turn Melterlights off
+            foreach (Light light in MelterLightsContainer.GetComponentsInChildren<Light>())
+            {
+                light.intensity = 1f;
+                light.color = Color.red;
+            }
         }
     }
 
