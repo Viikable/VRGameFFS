11c4770b463ba6a7bf1dd9e6cc7cd1da6f946123
Viikable <tehkovarma@hotmail.com>
2020-06-24 19:17:46 +0300

    more work on oxygen system

--- VREpisode1/Assets/OwnStuff/Scripts/BonsaiRoomScripts/OxygenControl.cs
+++ VREpisode1/Assets/OwnStuff/Scripts/BonsaiRoomScripts/OxygenControl.cs
@@ -15,29 +15,44 @@ public class OxygenControl : MonoBehaviour {
 
     [Tooltip("Tells the name of the previous oxygen level (Safe, Okay, Alarming or Deadly) in order to be able to play breathing sounds when the level changes only")]
     OxygenLevelName previousOxygenLevel;
+ 
+    [Tooltip("The factorial for the current oxygen level name which tells how much more the player's oxygen decreases per second when the oxygen level is constantly lowering. This value is different for each oxygen level name.")]
+    private float oxygenLevelLowersFactorial;
 
-    [Tooltip("Tells the base amount of time respective to the oxygen level as a default")]
-    float survivalTime;
+    [Tooltip("The factorial for the current oxygen level name which tells how much more the player's oxygen increases per second when the oxygen level is constantly lowering. This value is different for each oxygen level name.")]
+    private float oxygenLevelIncreasesFactorial;
 
-    [Tooltip("The factorial for a room which tells how much more the player's oxygen decreases per second when the oxygen level is constantly lowering. This value is different for each oxygen level name.")]
-    float oxygenLevelLowersFactorial;
-
-    [Tooltip("The factorial for a room which tells how much more the player's oxygen increases per second when the oxygen level is constantly lowering. This value is different for each oxygen level name.")]
-    float oxygenLevelIncreasesFactorial;
-
-    [Tooltip("The speed of oxygen percentage increasing in the room based on the room size")]
-    float accelerationSpeed;
+    [Tooltip("The factorial for the current oxygen level name which tells how much more the player's oxygen decreases per second when the oxygen level stays the same. This value is different for each oxygen level name.")]
+    private float oxygenLevelStaysFactorial;
 
     [Tooltip("Tells how quickly the oxygen divides itself between rooms when a door is opened to another space")]
-    float oxygenSpreadSpeed;
+    private float oxygenSpreadSpeed;
 
     [Tooltip("Tells the amount of oxygen the player currently has remaining")]
-    float playerOxygen;
+    private float playerOxygen;
 
     [Tooltip("Shows whether it is time to call the checkFunctions or not (second passed or not)")]
-    bool secondPassed;
+    private bool secondPassed;
 
-   //these are the four possible oxygen levels in the room
+    [Tooltip("Tells the amount of oxygen in MF lobby.")]
+    private float mainFacilityLobbyOxygen;
+
+    [Tooltip("Tells the amount of oxygen in MF Bridge.")]
+    private float mainFacilityBridgeOxygen;
+
+    [Tooltip("Tells the amount of oxygen in Bonsai Room.")]
+    private float bonsaiRoomOxygen;
+
+    [Tooltip("Tells the amount of oxygen in Janitor Room.")]
+    private float janitorRoomOxygen;
+
+    [Tooltip("Tells the amount of oxygen in Maintenance Corridor.")]
+    private float maintenanceCorridorOxygen;
+
+    [Tooltip("Tells the amount of oxygen in Melter Room.")]
+    private float melterRoomOxygen;
+
+    //these are the four possible oxygen levels in an area
     public enum OxygenLevelName
     {
         Safe,
@@ -50,10 +65,17 @@ public class OxygenControl : MonoBehaviour {
     {
         oxygenSpreadSpeed = 0.02f;
         currentOxygenLevel = OxygenLevelName.Safe;
-        playerOxygen = Mathf.Infinity;
+        playerOxygen = 120f;
         currentRoomOxygenPercentage = 100;
         previousRoomOxygenPercentage = 100;
         secondPassed = true;
+
+        mainFacilityBridgeOxygen = 0f;
+        mainFacilityLobbyOxygen = 0f;
+        bonsaiRoomOxygen = 0f;
+        janitorRoomOxygen = 0f;
+        maintenanceCorridorOxygen = 0f;
+        melterRoomOxygen = 0f;
     }
 
 
@@ -67,6 +89,21 @@ public class OxygenControl : MonoBehaviour {
             CheckCurrentOxygenLevelName();
             OxygenLevelChanges();
             StartCoroutine("WaitASecond");
+        }
+    }
+    //This method will be called from the Bonsai oxygen Machine when setting the new oxygen level
+    public void SetOxygenLevels(Color firstLight, Color secondLight, Color thirdLight, Color fourthLight)
+    {
+        // if all lights are green for example, which means MF lobby gets 80% oxygen
+        if (firstLight == Color.green && secondLight == Color.green && thirdLight == Color.green && fourthLight == Color.green)
+        {
+            mainFacilityLobbyOxygen = 80f;
+
+            mainFacilityBridgeOxygen = 0f;          
+            bonsaiRoomOxygen = 0f;
+            janitorRoomOxygen = 0f;
+            maintenanceCorridorOxygen = 0f;
+            melterRoomOxygen = 0f;
         }
     }
 
@@ -86,50 +123,52 @@ public class OxygenControl : MonoBehaviour {
 
         if (currentRoomOxygenPercentage > 75f)
         {
+            //resets oxygen level to max if this level is reached
             currentOxygenLevel = OxygenLevelName.Safe;
-            survivalTime = 120f;
-            oxygenLevelLowersFactorial = 0f;
-            oxygenLevelIncreasesFactorial = 0f;
+            playerOxygen = 120f;         
         }
         else if (currentRoomOxygenPercentage <= 75f && currentRoomOxygenPercentage >= 50f)
         {
             currentOxygenLevel = OxygenLevelName.Okay;         
             oxygenLevelLowersFactorial = 0.25f;
             oxygenLevelIncreasesFactorial = 0.75f;
+            oxygenLevelStaysFactorial = 0f;
         }
         else if (currentRoomOxygenPercentage < 50f && currentRoomOxygenPercentage > 25f)
         {
             currentOxygenLevel = OxygenLevelName.Alarming;           
             oxygenLevelLowersFactorial = 0.5f;
             oxygenLevelIncreasesFactorial = 0.5f;
+            oxygenLevelStaysFactorial = 0.25f;
         }
         else if (currentRoomOxygenPercentage <= 25f)
         {
             currentOxygenLevel = OxygenLevelName.Deadly;            
             oxygenLevelLowersFactorial = 0.75f;
             oxygenLevelIncreasesFactorial = 0.25f;
+            oxygenLevelStaysFactorial = 0.5f;
         }
     }
     // the changing speed of oxygen levels only affects how quickly the OxygenLevelName changes, the individual changing speed does not affect otherwise to the player's remaining oxygen
     private void OxygenLevelChanges()
     {
-        //player's oxygen changing speed if oxygen percentage in the room is currently changing
+        //player's oxygen changing speed if oxygen percentage in the room is currently changing and not Safe
         if (previousRoomOxygenPercentage != currentRoomOxygenPercentage && currentOxygenLevel != OxygenLevelName.Safe)
         {
             if (currentRoomOxygenPercentage < previousRoomOxygenPercentage)
-            {
-                // take into account the room size
-                playerOxygen -= 1 + oxygenLevelLowersFactorial;
+            {              
+                playerOxygen -= 1 + oxygenLevelLowersFactorial + oxygenLevelStaysFactorial;
             }
             else if (currentRoomOxygenPercentage > previousRoomOxygenPercentage)
             {               
-                playerOxygen -= 1 - oxygenLevelIncreasesFactorial;             
+                playerOxygen -= (1 - oxygenLevelIncreasesFactorial) + oxygenLevelStaysFactorial;             
             }
         }
-        //here we check the player's oxygen changing speed if the oxygen percentage in the room is not changing currently
+        //here we check the player's oxygen changing speed if the oxygen percentage in the room is not changing currently and not Safe
         else if (previousRoomOxygenPercentage == currentRoomOxygenPercentage && currentOxygenLevel != OxygenLevelName.Safe)
         {
-
+            //this causes for example that the oxygen in a Deadly area drains 50% faster than in an Okay area
+            playerOxygen -= 1 + oxygenLevelStaysFactorial;
         }
     }
 
