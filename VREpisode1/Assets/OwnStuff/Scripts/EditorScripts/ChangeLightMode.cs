1bd8ac2c9fc51a3196fc2685f0317c9f94b37a7e
Viikable <tehkovarma@hotmail.com>
2020-11-15 19:06:34 +0200

    lightmap reconfigured, added only occlusion culling and lightmap in the static settings

---
+++ VREpisode1/Assets/OwnStuff/Scripts/EditorScripts/ChangeLightMode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+#if UNITY_EDITOR
+public class ChangeLightMode {
+
+    [MenuItem("Omat scriptit/ChangeLightMode")]
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+#endif
